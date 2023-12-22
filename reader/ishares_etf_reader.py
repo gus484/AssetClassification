17077732dac125780b496bc6dcf2a50206b575a8
Markus <gustav484@web.de>
2023-12-22 21:55:18 +0100

    #11 use config files

--- reader/ishares_etf_reader.py
+++ reader/ishares_etf_reader.py
@@ -1,60 +1,74 @@
-import csv
+import logging
 import os
-from locale import atof
 
 from reader.asset import Asset, Value
 from reader.etf_reader import EtfReader, FundFamily, LocationCodes
 
+log = logging.getLogger("ac")
+
 
 class ISharesEtfReader(EtfReader):
-    REGEX = r'[A-Z0-9]{4}\_(holdings)\.(csv)'
+    CONFIGS = {}
 
-    def __init__(self, fpath: str):
-        super().__init__(fpath)
+    def __init__(self, fpath: str, config_name: str = None):
+        super().__init__(fpath, config_name)
         self.fund_family = FundFamily.ISHARES.value
-        self.start_row = 4
-        self.ticker_col = 0
-        self.name_col = 1
-        self.weight_col = 5
-        self.region_col = 9
+
+    def find_config(self) -> bool:
+        if self.config_name:
+            return True
+
+        for cfg_name, config in ISharesEtfReader.CONFIGS.items():
+            line = int(config["SUB_DETECTION"]["row"])
+            col = int(config["SUB_DETECTION"]["col"])
+            val = config["SUB_DETECTION"]["value"]
+
+            if val == self.get_data(line, col):
+                log.debug(f"... => {cfg_name}")
+                self.config_name = cfg_name
+                return True
+        return False
 
     def read_asset(self):
-        name = os.path.basename(os.path.normpath(self.fpath))
-        with open(self.fpath, newline='', encoding="utf8") as csvfile:
-            csv_reader = csv.reader(csvfile, delimiter=',', quotechar='"')
-            for line_no, line in enumerate(csv_reader, 1):
-                if line_no == 1:
-                    last_update = line[1]
+        self.open_file()
+        if not self.find_config():
+            return
 
+        self.init_from_config(ISharesEtfReader.CONFIGS[self.config_name])
+
+        file_name = os.path.basename(os.path.normpath(self.fpath))
+        date_obj = self.get_date()
+        last_update = date_obj.strftime('%d.%m.%Y')
+
+        '''      
                 if line_no == 3:
                     # normally there are 12 cols
                     if len(line) == 13:
                         self.weight_col += 1
                         self.region_col += 1
-                    break
-        date_obj = self.parse_date(last_update)
-        last_update = date_obj.strftime('%d.%m.%Y')
+                    break'''
 
-        self.isin = EtfReader.get_isin_from_file_name(self.fund_family, name)
-        name = EtfReader.get_name_from_isin(self.fund_family, self.isin)
+        self.isin = EtfReader.get_isin_from_file_name(self.fund_family, file_name)
+        if self.name_row:
+            name = self.get_data(self.name_row, self.name_col)
+        else:
+            name = EtfReader.get_name_from_isin(self.fund_family, self.isin)
+
         self.asset = Asset(name, self.isin, 0.0, last_update, [])
 
     def read_sheet(self):
-        with open(self.fpath, newline='', encoding="utf8") as csvfile:
-            csv_reader = csv.reader(csvfile, delimiter=',', quotechar='"')
-            for line_no, line in enumerate(csv_reader, 1):
-                if line_no < 4:
-                    continue
-                if self.name_col >= len(line):
-                    break
-                name = line[self.name_col]
+        for line_nbr in range(self.start_row, self.get_row_count()):
+            line = self.raw_data[line_nbr]
 
-                weight = line[self.weight_col]
-                weight = atof(weight.replace("%", "").replace("\xa0", ""))
-                ticker = line[self.ticker_col]
-                region = line[self.region_col]
-                region = EtfReader.get_region_code(LocationCodes.DE_FULL_NAME, region)
-                a = Value(name, weight, weight, ticker, region)
+            if self.weight_col >= len(line):
+                break
 
-                self.update_region(region, weight)
-                self.asset.values.append(a)
+            ticker = line[self.ticker_col]
+            name = line[self.holding_name_col]
+            weight = self.convert_str_to_float(line[self.weight_col])
+            region = line[self.region_col]
+            region = EtfReader.get_region_code(LocationCodes[self.location_code], region)
+            a = Value(name, weight, weight, ticker, region)
+
+            self.update_region(region, weight)
+            self.asset.values.append(a)
